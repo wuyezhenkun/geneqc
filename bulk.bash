867ca529599d1fdfc156cbaa2a855f053a881558
WeiliangBMBL <liu.3540@osu.edu>
2019-05-10 06:30:55 -0400

    Update bulk.bash

--- bulk.bash
+++ bulk.bash
@@ -14,7 +14,7 @@ module load anaconda5/5.0.0-3.6
 
 python extract_transcript_seq_gff.py GCF_000001405.37_GRCh38.p11_genomic.fna GCF_000001405.37_GRCh38.p11_genomic.gff human_transcripts_seq.fa human_transcripts_seq.gff
 
-module load hisat2
+module load hisat2/2.0.4
 hisat2-build -f human_transcripts_seq.fa ./hisatindex/Humo
 hisat2 -x ./hisatindex/Humo -k 10 -p 40 -1 SRR6029567_1.fastq -2 SRR6029567_2.fastq -S SRR6029567.bam
 
