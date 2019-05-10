08a10aa56dffee6a7ff8f50efa810530ca6a23c5
WeiliangBMBL <liu.3540@osu.edu>
2019-05-10 00:46:29 -0400

    Update Plant_Genome.bash

--- Plant_Genome.bash
+++ Plant_Genome.bash
@@ -6,11 +6,11 @@
 # echo commands to stdout 
 set –x
 
-module load anaconda5/5.0.0-3.6
 
 
 ##data path: /pylon5/cc5fpcp/weiliang/BMBL/GeneQC/Plant_Genome/
-##program path: /home/weiliang/BMBL/GeneQC-master/GeneQC_Python/
+##program path: /home/weiliang/BMBL/GeneQC-master/GeneQC_Python
 
-
-python /home/weiliang/BMBL/GeneQC-master/GeneQC_Python/GeneQC2.py 1 /pylon5/cc5fpcp/weiliang/BMBL/GeneQC/Plant_Genome/Athaliana_167_TAIR9.fa /pylon5/cc5fpcp/weiliang/BMBL/GeneQC/Plant_Genome/Athaliana_167_TAIR10.gene.gff3 /pylon5/cc5fpcp/weiliang/BMBL/GeneQC/Plant_Genome/SRR3305038.bam
+cd /home/weiliang/BMBL/GeneQC-master/GeneQC_Python
+module load anaconda5/5.0.0-3.6
+python GeneQC2.py 1 /pylon5/cc5fpcp/weiliang/BMBL/GeneQC/Plant_Genome/Athaliana_167_TAIR9.fa /pylon5/cc5fpcp/weiliang/BMBL/GeneQC/Plant_Genome/Athaliana_167_TAIR10.gene.gff3 /pylon5/cc5fpcp/weiliang/BMBL/GeneQC/Plant_Genome/SRR3305038.bam
